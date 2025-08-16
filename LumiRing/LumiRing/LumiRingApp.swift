f08e5f69a142f82b9dfcb022662849a5f124cb45
Isam6 <issamdiez10@gmail.com>
2025-08-16 15:29:48 +0200

    Adding iOS App project

---
+++ LumiRing/LumiRing/LumiRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiRingApp.swift
+//  LumiRing
+//
+//  Created by Isam El Mourabet on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LumiRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
