8955952d4c887cea137b12dcf14119e2d4f5522d
Isam6 <issamdiez10@gmail.com>
2025-08-16 22:51:29 +0200

    Create DoorBackendApplication.java

---
+++ LumiRing-API/src/main/java/com/example/Door/DoorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Door;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorBackendApplication.class, args);
+	}
+
+}
