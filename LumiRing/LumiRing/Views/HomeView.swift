bb42775237818048931c42411a7215d8059d0ea1
Isam Mourabet <74101543+isamelmourabet@users.noreply.github.com>
2025-08-18 21:04:01 +0200

    fix: iOS UI bugs (#9)
    * Minor changes
    
    * Create HomeView.swift
    
    * Update ContentView.swift
    
    * Update HomeView.swift
    
    * Update HomeView.swift
    
    * Update HomeView.swift
    
    * Update HomeView.swift
    
    * Update gitignore
    
    * Update .gitignore
    
    * Update gitignore

--- LumiRing/LumiRing/Views/HomeView.swift
+++ LumiRing/LumiRing/Views/HomeView.swift
@@ -17,6 +17,10 @@ struct HomeView: View {
         NavigationStack {
             ZStack {
                 Color.blue.opacity(0.5)
+<<<<<<< HEAD
+                
+=======
+>>>>>>> dev
                 VStack {
                     if !isConected {
                         Text("No se ha encontrado el dispositivo")
@@ -29,6 +33,10 @@ struct HomeView: View {
                     .buttonStyle(.borderedProminent)
                     .padding()
                     
+<<<<<<< HEAD
+                    
+=======
+>>>>>>> dev
                     if isConected {
                         VStack  {
                             HStack {
@@ -83,7 +91,10 @@ struct HomeView: View {
                                                         isCalling = true
                                                     }
                                                 }
+<<<<<<< HEAD
+=======
                                             //.offset(x: -10)
+>>>>>>> dev
                                         }
                                         .padding()
                                     }
@@ -91,15 +102,38 @@ struct HomeView: View {
                                 } else {
                                     // Fallback on earlier versions
                                     HStack {
+<<<<<<< HEAD
+                                        if isCalling {
+                                            Image(systemName: "phone.down.fill")
+                                                .frame(width: 40, height: 40)
+                                                .background(.secondary)
+                                                .clipShape(Circle())
+                                                .onTapGesture {
+                                                    withAnimation() {
+                                                        isCalling = false
+                                                    }
+                                                }
+                                                
+                                        }
+=======
                                         Image(systemName: "phone.down.fill")
                                             .frame(width: 40, height: 40)
                                             .background(.secondary)
                                             .clipShape(Circle())
+>>>>>>> dev
                                         
                                         Image(systemName: "phone.fill")
                                             .frame(width: 40, height: 40)
                                             .background(.secondary)
                                             .clipShape(Circle())
+<<<<<<< HEAD
+                                            .onTapGesture {
+                                                withAnimation() {
+                                                    isCalling = true
+                                                }
+                                            }
+=======
+>>>>>>> dev
                                     }
                                     .padding()
                                 }
@@ -116,7 +150,11 @@ struct HomeView: View {
                                                 isLocked.toggle()
                                             }
                                             
+<<<<<<< HEAD
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+=======
                                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+>>>>>>> dev
                                                 withAnimation {
                                                     isLocked.toggle()
                                                 }                                            }
@@ -124,18 +162,41 @@ struct HomeView: View {
                 
                                 } else {
                                     // Fallback on earlier versions
+<<<<<<< HEAD
+                                    Image(systemName: isLocked ? "lock.fill" : "lock.open.fill")
+=======
                                     Image(systemName: "lock.fill")
+>>>>>>> dev
                                         .frame(width: 40, height: 40)
                                         .background(.secondary)
                                         .clipShape(Circle())
                                         .padding()
+<<<<<<< HEAD
+                                        .onTapGesture {
+                                            withAnimation(.bouncy) {
+                                                isLocked.toggle()
+                                            }
+                                            
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                                                withAnimation {
+                                                    isLocked.toggle()
+                                                }                                            }
+                                        }
+=======
+>>>>>>> dev
                                 }
                             }
                             
                         }
                         .frame(width: 200, height: 200)
                         .background(Color.blue)
+<<<<<<< HEAD
+                        .shadow(radius: 10, x: 5, y: 5)
                         .clipShape(RoundedRectangle(cornerRadius: 25))
+                        .padding()
+=======
+                        .clipShape(RoundedRectangle(cornerRadius: 25))
+>>>>>>> dev
                     }
                 }
             }
